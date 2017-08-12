3ce8db9a4897d0dd3fff1dd39d17e3f55f0cfd2a
Cody Landry <Cody.landry@me.com>
2017-08-11 23:10:24 -0400

    added example scripts file

--- dev/scripts.py
+++ dev/scripts.py
@@ -5,5 +5,4 @@ def test():
 	git_root = autonotes.get_git_root_path(os.getcwd())
 	repo = git.Repo(git_root)
 	latest_commit = next((c.message for c in repo.iter_commits()))
-	autonotes.add_item(latest_commit, section='notes', checkbox=True, checked=True)
-
+	print latest_commit
