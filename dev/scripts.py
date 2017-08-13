5ad466325c9955906fd95dcc37c04ff4a939ecdc
Cody Landry <Cody.landry@me.com>
2017-08-13 00:47:13 -0400

    modifying test dev scripts

--- dev/scripts.py
+++ dev/scripts.py
@@ -3,6 +3,7 @@ from core import Core
 
 @autonotes.hook()
 def test(repo):
-	with Core(os.getcwd()) as core:
+	this_dir = os.path.dirname(os.path.abspath(__file__))
+	with Core(this_dir) as core:
 		latest_commit = next((c.message for c in repo.iter_commits()))
 		core.add_item('features', latest_commit, checkbox=True, checked=True)
